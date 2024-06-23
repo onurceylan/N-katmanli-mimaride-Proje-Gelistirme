ead0a055b4f816f37897416f35cc5b2f32ebc1da
onurceylan <onurceylan832@gmail.com>
2024-06-23 11:37:01 +0300

    added entitylayer correctly and fixed

---
+++ EntityLayer/EntityPersonel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer
+{
+    public class EntityPersonel
+    {
+        private int id;
+        private string ad;
+        private string soyad;
+        private string sehir;
+        private short maas; // smallint == short
+        private string gorev;
+
+        // Encapsulation
+        public int Id { get => id; set => id = value; } // properties
+        public string Ad { get => ad; set => ad = value; }
+        public string Soyad { get => soyad; set => soyad = value; }
+        public string Sehir { get => sehir; set => sehir = value; }
+        public short Maas { get => maas; set => maas = value; }
+        public string Gorev { get => gorev; set => gorev = value; }
+    }
+}
